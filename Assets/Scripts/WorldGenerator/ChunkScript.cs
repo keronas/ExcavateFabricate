c34fc2f325b561b28c74288ec751011754782859
keronas <keronas@seznam.cz>
2021-07-09 23:29:32 +0200

    Add basic world generation

--- Assets/Scripts/WorldGenerator/ChunkScript.cs
+++ Assets/Scripts/WorldGenerator/ChunkScript.cs
@@ -1,19 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using LibNoise.Unity;
+using LibNoise.Unity.Generator;
 using UnityEngine;
 
 public class ChunkScript : MonoBehaviour
 {
     public GameObject BlockPrefab;
     public uint ChunkSize;
-    public Vector2 Position;
+    public double PerlinWeight;
+    public double HeightWeight;
+    public double GroundLevel;
+    public bool OptimizeBlocks;
+    public Perlin PerlinGenerator;
 
 
     private byte[][][] data { get; set; }
     private List<GameObject> objects { get; set; } = new List<GameObject>();
 
     // Start is called before the first frame update
-    void Start()
+    void Start()  
+    {
+        InitializeData();
+        InstantiateBlocks();
+    }
+
+    private void InitializeData()
+    {
+        data = new byte[ChunkSize][][];
+        for (var x = 0; x < ChunkSize; x++)
+        {
+            data[x] = new byte[ChunkSize][];
+            for (var y = 0; y < ChunkSize; y++)
+            {
+                data[x][y] = new byte[ChunkSize];
+                for (var z = 0; z < ChunkSize; z++)
+                {
+                    var localPosition = new Vector3(x, y, z);
+                    var worldPosition = transform.TransformPoint(localPosition);
+                    var perlinPositionMultiplier = 0.04f;
+                    var perlinValue = PerlinGenerator.GetValue(worldPosition * perlinPositionMultiplier);
+                    var heightValue = GroundLevel - worldPosition.y;
+
+                    if (perlinValue * PerlinWeight + heightValue * HeightWeight > 1) 
+                    {
+                        data[x][y][z] = 1;
+                    }
+                    else
+                    {
+                        data[x][y][z] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private void InstantiateBlocks()
     {
         for (var x = 0; x < ChunkSize; x++)
         {
@@ -21,8 +63,21 @@ public class ChunkScript : MonoBehaviour
             {
                 for (var z = 0; z < ChunkSize; z++)
                 {
-                    if (Random.value > 0.5)
-                        CreateBlock(new Vector3(x, y, z));
+                    if (data[x][y][z] != 0)
+                    {
+                        // If any neighbouring space is still inside chunk but doesn't contain any block, then current block may be visible
+                        if (!OptimizeBlocks ||
+                            (x - 1 >= 0 && data[x - 1][y][z] == 0) ||
+                            (y - 1 >= 0 && data[x][y - 1][z] == 0) ||
+                            (z - 1 >= 0 && data[x][y][z - 1] == 0) ||
+                            (x + 1 < ChunkSize && data[x + 1][y][z] == 0) ||
+                            (y + 1 < ChunkSize && data[x][y + 1][z] == 0) ||
+                            (z + 1 < ChunkSize && data[x][y][z + 1] == 0))
+                        {
+                            CreateBlock(new Vector3(x, y, z));
+                        }
+                    }
+                    
                 }
             }
         }
@@ -34,10 +89,11 @@ public class ChunkScript : MonoBehaviour
         
     }
 
-    void CreateBlock(Vector3 position)
+    private void CreateBlock(Vector3 position)
     {
         var positionOffset = new Vector3(0.5f, 0.5f, 0.5f); // to fit nicely into the editor grid
-        var block = GameObject.Instantiate(BlockPrefab, position + positionOffset, new Quaternion(), transform);
+        var block = GameObject.Instantiate(BlockPrefab, transform);
+        block.transform.localPosition = position + positionOffset;
         block.name = $"Block {position.x};{position.y};{position.z}";
         objects.Add(block);
     }
--- Assets/Scripts/WorldGenerator/WorldGeneratorScript.cs
+++ Assets/Scripts/WorldGenerator/WorldGeneratorScript.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using LibNoise.Unity.Generator;
 using UnityEngine;
 
 public class WorldGeneratorScript : MonoBehaviour
 {
     public GameObject BlockPrefab;
     public uint ChunkSize;
+    public double PerlinWeight;
+    public double HeightWeight;
+    public double GroundLevel;
+    public bool OptimizeBlocks;
 
+    private Dictionary<Vector3Int, GameObject> chunks = new Dictionary<Vector3Int, GameObject>();
+
+    private Perlin perlinGenerator = new Perlin();
 
     // Start is called before the first frame update
     void Start()
     {
-        var chunk = new GameObject("TestChunk");
-        var chunkScript = chunk.AddComponent<ChunkScript>();
-        chunkScript.BlockPrefab = BlockPrefab;
-        chunkScript.ChunkSize = ChunkSize;
-        chunkScript.Position = Vector2.zero;
+        for (var x = 0; x < 5; x++)
+        {
+            for (var y = 0; y < 2; y++)
+            {
+                for (var z = 0; z < 5; z++)
+                {
+                    CreateChunk(new Vector3Int(x, y, z));
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    private void CreateChunk(Vector3Int position)
+    {
+        var chunk = new GameObject($"Chunk {position.x};{position.y};{position.z}");
+        chunk.transform.position = (Vector3)position * ChunkSize;
+        var chunkScript = chunk.AddComponent<ChunkScript>();
+        chunkScript.BlockPrefab = BlockPrefab;
+        chunkScript.ChunkSize = ChunkSize;
+        chunkScript.PerlinWeight = PerlinWeight;
+        chunkScript.HeightWeight = HeightWeight;
+        chunkScript.GroundLevel = GroundLevel;
+        chunkScript.OptimizeBlocks = OptimizeBlocks;
+        chunkScript.PerlinGenerator = perlinGenerator;
+        chunks.Add(position, chunk);
     }
 }
