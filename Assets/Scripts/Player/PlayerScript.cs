8f13f1b9c6b0a315dbfe8ef1d1d28c39b15ca772
keronas <keronas@seznam.cz>
2021-07-15 16:06:01 +0200

    Simplify identifying blocks to remove

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -59,23 +59,9 @@ public class PlayerScript : MonoBehaviour
         {
             var normal = hitInfo.normal;
             var point = hitInfo.point;
-            Vector3Int blockPosition;
 
-            // Ceil or Floor decides which side of the face is the block to be removed, as that coordinate is always halfway between two blocks
-            if (normal.x > 0.1)
-                blockPosition = new Vector3Int(Mathf.FloorToInt(point.x), Mathf.RoundToInt(point.y), Mathf.RoundToInt(point.z));
-            else if (normal.x < -0.1)
-                blockPosition = new Vector3Int(Mathf.CeilToInt(point.x), Mathf.RoundToInt(point.y), Mathf.RoundToInt(point.z));
-            else if (normal.y > 0.1)
-                blockPosition = new Vector3Int(Mathf.RoundToInt(point.x), Mathf.FloorToInt(point.y), Mathf.RoundToInt(point.z));
-            else if (normal.y < -0.1)
-                blockPosition = new Vector3Int(Mathf.RoundToInt(point.x), Mathf.CeilToInt(point.y), Mathf.RoundToInt(point.z));
-            else if (normal.z > 0.1)
-                blockPosition = new Vector3Int(Mathf.RoundToInt(point.x), Mathf.RoundToInt(point.y), Mathf.FloorToInt(point.z));
-            else if (normal.z < -0.1)
-                blockPosition = new Vector3Int(Mathf.RoundToInt(point.x), Mathf.RoundToInt(point.y), Mathf.CeilToInt(point.z));
-            else
-                throw new Exception("Raycast hit has no normal.");
+            var correctedPoint = point - normal / 2; // moves point from face of the block to the middle of it, to decide which side of face is the correct block 
+            var blockPosition = Vector3Int.RoundToInt(correctedPoint);
 
             var chunk = hitInfo.collider.gameObject.GetComponent<ChunkScript>();
             chunk.RemoveBlock(blockPosition);
